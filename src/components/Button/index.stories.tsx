ed486a19b0785e6d1e0e331b65d0ad8908e051a1
xdigu <xdigu@live.com>
2021-07-19 22:10:37 -0300

    change components to src

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -7,9 +7,7 @@ export default {
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => (
-  <Button {...args} />
-);
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Number = Template.bind({});
 Number.args = {
